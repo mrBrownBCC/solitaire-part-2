eabc66fcdc245e8a7d480442525039cc2fb9a339
Everett Brown <everett_a_brown2@mcpsmd.org>
2024-11-11 06:57:27 -0500

    Initial commit

---
+++ solitaire-part-1/src/main/java/com/example/App.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import javax.swing.*;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class App {
+    public static void main(String[] args) { 
+      //add your code here!
+
+
+      
+    }
+}
